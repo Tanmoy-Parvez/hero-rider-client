1229209353b52caa06ae287d5e6bfbefe589c15e
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-12-31 20:55:07 +0600

    rider added

--- src/Pages/RiderProfile/RiderProfile.js
+++ src/Pages/RiderProfile/RiderProfile.js
@@ -6,13 +6,48 @@ const RiderProfile = () => {
     const [rider, setRider] = useState({});
 
     useEffect(() => {
-
+        fetch(`http://localhost:5000/saveUser/${user?.email}`)
+            .then(res => res.json())
+            .then(data => {
+                setRider(data)
+            })
     },
         [])
     return (
-        <div>
+        <div className="container text-center my-5">
             <h1>Rider Profile</h1>
-        </div>
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-8 shadow-lg rounded-3 p-5">
+                    <img src={rider?.profileImg} alt="" className="w-25 mx-auto rounded-circle" />
+                    <div className="row">
+                        <div className="col-md-3">
+                            <div className="text-start ps-3">
+                                <h4>Name</h4>
+                                <h4>Email</h4>
+                                <h4>Phone</h4>
+                                <h4>Address</h4>
+                                <h4>Age</h4>
+                                <h4>Vehicle</h4>
+                                <h4>Vehicle Info</h4>
+                            </div>
+                        </div>
+                        <div className="col-md-9">
+                            <div className="text-start">
+                                <h4>: {rider?.name}</h4>
+                                <h4>: {rider?.email}</h4>
+                                <h4>: {rider?.phone}</h4>
+                                <h4>: {rider?.address}</h4>
+                                <h4>: {rider?.age} years</h4>
+                                <h4>: {rider?.vehicle}</h4>
+                                <h4>: {rider?.carInfo}</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-2"></div>
+        </div >
     );
 };
 
